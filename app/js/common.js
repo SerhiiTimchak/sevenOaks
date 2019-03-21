aa6d67d72c8c212e7f315ca48c5b80e57ae3ba51
SerhiiTimchak <2freemangordon3@gmail.com>
2019-03-21 19:02:50 +0200

    working carusel but have problems with button positioning

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,6 @@
 window.onload = function() {
+
+    // Carusel START 
     let images = document.querySelectorAll('#slider .images img');
     let counter = 0;
     let btnPrev = document.getElementById('btn-left');
@@ -20,5 +22,47 @@ window.onload = function() {
             counter = 0;
         }
         images[counter].className = 'active';
-    }
-}
+    };
+    // Carusel END
+
+    //Buttons Height START
+
+    var caruselImgActive = document.querySelector(".active");
+    var caruselImgButton = document.querySelector("#btn-left");
+    var caruselButtonsLine = document.querySelector(".buttons");
+    var caruselImgStyle = window.getComputedStyle(caruselImgActive);
+    var caruselButtonStyle = window.getComputedStyle(caruselImgButton);
+    var caruselImgHeight = caruselImgStyle.getPropertyValue('height');
+    var caruselButtonHeight = caruselButtonStyle.getPropertyValue('height');
+
+    // console.log(typeof(+caruselImgHeight), typeof(+caruselButtonHeight));
+
+
+    function calculateCenter(imgHeight, buttonHeight) {
+        console.log(parseInt(caruselImgHeight), parseInt(caruselButtonHeight));
+        let result = (parseInt(caruselImgHeight) - parseInt(caruselButtonHeight)) / 2;
+        return result;
+    };
+
+    function setButtonsPosition(result, buttonsLine) {
+        buttonsLine.style.visibility = 'visible';
+        buttonsLine.style.top = result + 'px';
+    };
+
+    // console.log(calculateCenter(caruselImgHeight, caruselButtonHeight));
+    setButtonsPosition(calculateCenter(caruselImgHeight, caruselButtonHeight), caruselButtonsLine);
+
+    window.onresize = function() {
+        caruselImgActive = document.querySelector(".active");
+        caruselImgButton = document.querySelector("#btn-left");
+        caruselButtonsLine = document.querySelector(".buttons");
+        caruselImgStyle = window.getComputedStyle(caruselImgActive);
+        caruselButtonStyle = window.getComputedStyle(caruselImgButton);
+        caruselImgHeight = caruselImgStyle.getPropertyValue('height');
+        caruselButtonHeight = caruselButtonStyle.getPropertyValue('height');
+
+        setButtonsPosition(calculateCenter(caruselImgHeight, caruselButtonHeight), caruselButtonsLine);
+
+    };
+    //Buttons Height END
+};
