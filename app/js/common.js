ac6cdb9c4cf673572434d4df8ceb6cd27735195e
SerhiiTimchak <2freemangordon3@gmail.com>
2019-03-24 20:47:12 +0200

    completely ready content part

--- app/js/common.js
+++ app/js/common.js
@@ -6,6 +6,7 @@ window.onload = function() {
     let btnPrev = document.getElementById('btn-left');
     let btnNext = document.getElementById('btn-right');
     btnPrev.onclick = function() {
+        images[counter].className = 'transp';
         images[counter].className = '';
         counter--;
         if (counter < 0) {
@@ -16,6 +17,7 @@ window.onload = function() {
     };
 
     btnNext.onclick = function() {
+        images[counter].className = 'transp';
         images[counter].className = '';
         counter++;
         if (counter >= images.length) {
@@ -27,6 +29,14 @@ window.onload = function() {
 
     //Buttons Height START
 
+    // var caruselImgActive = document.querySelector(".active");
+    // var caruselImgButton = document.querySelector("#btn-left");
+    // var caruselButtonsLine = document.querySelector(".buttons");
+    // var caruselImgStyle = window.getComputedStyle(caruselImgActive);
+    // var caruselImgHeight = caruselImgStyle.getPropertyValue('height');
+    // var caruselButtonHeight = caruselImgButton.offsetHeight;
+
+    var sliderBlock = document.querySelector("#slider");
     var caruselImgActive = document.querySelector(".active");
     var caruselImgButton = document.querySelector("#btn-left");
     var caruselButtonsLine = document.querySelector(".buttons");
@@ -34,6 +44,10 @@ window.onload = function() {
     var caruselImgHeight = caruselImgStyle.getPropertyValue('height');
     var caruselButtonHeight = caruselImgButton.offsetHeight;
 
+    function setSliderBlockDiamentions(imageHeight, sliderBlock) {
+        let height = parseInt(imageHeight);
+        sliderBlock.style.height = height + 'px';
+    }
 
     function calculateCenter(imgHeight, buttonHeight) {
         let result = (parseInt(caruselImgHeight) - parseInt(caruselButtonHeight)) / 2;
@@ -50,21 +64,23 @@ window.onload = function() {
 
     function positionWelcomeBlock(block, imageHeight) {
         var result = parseInt(caruselImgHeight) * 0.6230769230769231;
-        console.log(result);
         block.style.top = result + 'px';
-
     }
     positionWelcomeBlock(welcomeBlock, caruselImgHeight);
-
+    setSliderBlockDiamentions(caruselImgHeight, sliderBlock);
     setButtonsPosition(calculateCenter(caruselImgHeight, caruselButtonHeight), caruselButtonsLine);
 
     window.onresize = function() {
+
+        sliderBlock = document.querySelector("#slider");
         caruselImgActive = document.querySelector(".active");
         caruselImgButton = document.querySelector("#btn-left");
         caruselButtonsLine = document.querySelector(".buttons");
         caruselImgStyle = window.getComputedStyle(caruselImgActive);
         caruselImgHeight = caruselImgStyle.getPropertyValue('height');
         caruselButtonHeight = caruselImgButton.offsetHeight;
+
+        setSliderBlockDiamentions(caruselImgHeight, sliderBlock);
         setButtonsPosition(calculateCenter(caruselImgHeight, caruselButtonHeight), caruselButtonsLine);
         positionWelcomeBlock(welcomeBlock, caruselImgHeight);
 
